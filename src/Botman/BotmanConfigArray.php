ad1fa5bd322e16b34e62d369caff6b24bf314163
Fabian Schmengler <fs@integer-net.de>
2017-02-11 17:02:26 +0100

    Make BotmanConfigArray compatible with object manager

--- src/Botman/BotmanConfigArray.php
+++ src/Botman/BotmanConfigArray.php
@@ -9,6 +9,11 @@ namespace FireGento\MageBot\Botman;
  */
 class BotmanConfigArray extends \ArrayObject implements BotmanConfig
 {
+    public function __construct($data = [])
+    {
+        parent::__construct($data, 0, \ArrayIterator::class);
+    }
+
     public function toArray() : array
     {
         return $this->getArrayCopy();
