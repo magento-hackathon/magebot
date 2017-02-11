49410aee7a7278a07e544b22d41b58322b1a51e4
Fabian Schmengler <fs@integer-net.de>
2017-02-11 17:15:41 +0100

    Add bridge for botman to use Magento cache

---
+++ src/Botman/BotmanCacheMagento.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace FireGento\MageBot\Botman;
+
+use \Magento\Framework\App\CacheInterface as MagentoCache;
+use Mpociot\BotMan\Interfaces\CacheInterface as BotmanCache;
+
+class BotmanCacheMagento implements BotmanCache
+{
+    /** @var  MagentoCache */
+    private $magentoCache;
+
+    public function __construct(MagentoCache $magentoCache)
+    {
+        $this->magentoCache = $magentoCache;
+    }
+
+    public function has($key)
+    {
+        return $this->magentoCache->load($key) !== false;
+    }
+
+    public function get($key, $default = null)
+    {
+        if (! $this->has($key)) {
+            return $default;
+        }
+        return $this->magentoCache->load($key);
+    }
+
+    public function pull($key, $default = null)
+    {
+        $value = $this->get($key, $default);
+        $this->magentoCache->remove($key);
+        return $value;
+    }
+
+    public function put($key, $value, $minutes)
+    {
+        $this->magentoCache->save($value, $key, ['botman'], $minutes * 60);
+    }
+
+}
--- src/MageBot.php
+++ src/MageBot.php
@@ -5,6 +5,7 @@ namespace FireGento\MageBot;
 use FireGento\MageBot\Botman\BotmanConfig;
 use Mpociot\BotMan\BotMan;
 use Mpociot\BotMan\BotManFactory;
+use Mpociot\BotMan\Interfaces\CacheInterface;
 
 /**
  * Entry point for MageBot features (Magento independent)
@@ -15,15 +16,18 @@ class MageBot
 {
     /** @var BotmanConfig */
     private $config;
+    /** @var  CacheInterface */
+    private $cache;
 
-    public function __construct(BotmanConfig $config)
+    public function __construct(BotmanConfig $config, CacheInterface $cache)
     {
         $this->config = $config;
+        $this->cache = $cache;
     }
 
     public function start()
     {
-        $botman = BotManFactory::create($this->config->toArray());
+        $botman = BotManFactory::create($this->config->toArray(), $this->cache);
 
         $botman->hears('hello', function (BotMan $bot) {
             $bot->reply('Hello yourself. How cool is that?');
