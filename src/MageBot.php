ab7749c07e603b3d89296dfef4eb9c9e4d66f5f0
Fabian Schmengler <fs@integer-net.de>
2017-02-11 13:24:38 +0100

    Basic setup for Magento independent unit tests

---
+++ src/MageBot.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FireGento;
+
+/**
+ * Entry point for MageBot features (Magento independent)
+ *
+ * @package FireGento
+ */
+class MageBot
+{
+
+}
---
+++ tests/MageBotTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace FireGento;
+
+use PHPUnit\Framework\TestCase;
+
+class MageBotTest extends TestCase
+{
+    public function testCanInstantiateMagebot()
+    {
+        $mageBot = new MageBot();
+        $this->assertInstanceOf(MageBot::class, $mageBot);
+    }
+}
